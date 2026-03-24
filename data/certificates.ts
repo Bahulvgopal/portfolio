d2077d851f41f119f62d961b242f872359ea7e8d
Bahulvgopal <145193440+Bahulvgopal@users.noreply.github.com>
2026-03-24 15:55:18 +0530

    Update certificates.ts

--- data/certificates.ts
+++ data/certificates.ts
@@ -9,7 +9,7 @@ type Certificate = {
 export const certificates: Certificate[] = [  // ✅ IMPORTANT
   {
     title: "MERN Stack",
-    issuer: "Spectrum Solutions ,Eranakulam",
+    issuer: "Spectrum Solutions, Eranakulam",
     year: "01 May 2023 to 05 June 2023",
   },
   {
@@ -30,6 +30,27 @@ export const certificates: Certificate[] = [  // ✅ IMPORTANT
     issuer: "IEDC - KSUM",
     year: "25 February 2026",
     image: "/images/certificates/SS.jpg",
+    link: "https://example.com/certificate", // optional
+  },
+      {
+    title: "Cybersecurity Workshop",
+    issuer: "Offenso Academy",
+    year: "10 October 2025",
+    image: "/images/certificates/cyber.png",
+    link: "https://example.com/certificate", // optional
+  },
+      {
+    title: "Industrial Java Development Workshop",
+    issuer: "Spectrum Solutions, Ernakulam",
+    year: "29 June 2023",
+    image: "/images/certificates/ijd.png",
+    link: "https://example.com/certificate", // optional
+  },
+      {
+    title: "Configure And Deploy Web Server in Cloud",
+    issuer: "Spectrum Solutions, Ernakulam",
+    year: "12 August 2023",
+    image: "/images/certificates/web.png",
     link: "https://example.com/certificate", // optional
   },
 ];
