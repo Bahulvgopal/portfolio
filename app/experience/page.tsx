66d6f12f8a79d3c1b7e3150eb6fec1a734e055f3
Bahul V Gopal <bahulvgopal@gmail.com>
2026-08-14 19:32:31 +0530

    routes changed

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,6 +1,6 @@
 import ExperiencePage from "@/components/ExperiencePage";
 import { getExperiences } from "@/lib/api/experience";
-
+export const dynamic = "force-dynamic";
 export default async function Page() {
   const experiences = await getExperiences();
 
