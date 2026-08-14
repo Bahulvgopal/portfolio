84302dee611f142f3337ac4ba29825e3fa469e26
Bahul V Gopal <bahulvgopal@gmail.com>
2026-08-14 19:29:12 +0530

    routes changed

--- app/certificates/page.tsx
+++ app/certificates/page.tsx
@@ -1,6 +1,6 @@
 import { getCertificates } from "@/lib/api/certificates";
 import CertificatesPage from "@/components/CertificatesPage";
-
+export const dynamic = "force-dynamic";
 export default async function Page() {
   const certificates = await getCertificates();
 
