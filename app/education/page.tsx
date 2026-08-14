c8e54c88799731991ddbe4ba16dbac946db3582f
Bahul V Gopal <bahulvgopal@gmail.com>
2026-08-14 19:30:54 +0530

    routes changed

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -1,6 +1,6 @@
 import { getEducation } from "@/lib/api/education";
 import EducationPage from "@/components/EducationPage";
-
+export const dynamic = "force-dynamic";
 export default async function Page() {
   const education = await getEducation();
 
